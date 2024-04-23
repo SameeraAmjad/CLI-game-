34939e0c9bc7cf172c167485a93c24a7c0e68571
SameeraAmjad <mehaksameera20@gmail.com>
2024-04-23 21:46:50 +0500

    push my game to github

---
+++ index.js
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+// 1) Computer will generate a number 
+// 2) User input for guessing number 
+// 3) Compare user input with computer generated number and show results 
+const randomNumber = Math.floor(Math.random() * 1 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please Guess a Number",
+    },
+]);
+if (answers.userGuessedNumber === randomNumber) {
+    console.log("CONGRATULATION !!! You Guessed Right Number.");
+}
+else {
+    console.log("Oooppss You Guessed Wrong Number !!!.");
+}
+console.log(" GAME OVER ..");
